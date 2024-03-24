6c531ac4308328e76606ea2bc37b62df594fc7ae
Oleg Iarygin <oleg@arhadthedev.net>
2024-03-24 14:17:33 +0400

    Add ecma262compilers reading tests

---
+++ tests/conformance_ecma262compilers.rs
@@ -0,0 +1,41 @@
+use rstest::rstest;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+fn check_if_readable(path: PathBuf) {
+    read_to_string(&path).unwrap();
+}
+
+fn get_explicit_variant(default_variant: PathBuf) -> PathBuf {
+    let mut new_variant = default_variant.canonicalize().unwrap();
+    new_variant.pop();
+    new_variant.pop();
+    new_variant.push("pass-explicit");
+    new_variant.push(default_variant.file_name().unwrap());
+    return new_variant;
+}
+
+#[rstest]
+fn script_pass(
+    #[files("tests/_data/test262-parser-tests/pass/*.js")]
+    path: PathBuf,
+) {
+    check_if_readable(path.clone());
+    check_if_readable(get_explicit_variant(path));
+}
+
+#[rstest]
+fn script_fail(
+    #[files("tests/_data/test262-parser-tests/fail/*.js")]
+    path: PathBuf,
+) {
+    check_if_readable(path);
+}
+
+#[rstest]
+fn script_early_error(
+    #[files("tests/_data/test262-parser-tests/early/*.js")]
+    path: PathBuf,
+) {
+    check_if_readable(path);
+}
