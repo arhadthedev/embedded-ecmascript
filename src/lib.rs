9c6b2a85c0f3f2bec578414a04066c9bd98d3057
Oleg Iarygin <oleg@arhadthedev.net>
2024-04-13 21:19:04 +0300

    gh-15: Add match_ascii_letter function (#62)

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,7 @@ pub fn dummy() {
     _tokenizer::names::match_zwnj("");
     _tokenizer::names::match_zwj("");
     _tokenizer::names::match_reserved_word("");
+    _tokenizer::names::match_ascii_letter("");
     _tokenizer::names::match_unicode_id_start("");
     _tokenizer::names::match_unicode_id_continue("");
     _tokenizer::space::match_whitespace("");
