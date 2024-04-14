b3627872e4b8e6aa98e122409a9ddf2c4e95f2dd
Oleg Iarygin <oleg@arhadthedev.net>
2024-04-14 09:58:42 +0300

    gh-9: Add explanation of ECMA-262 grammar terminology (#66)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 //! Embed ECMAScript into desktop and automotive programs.
+//!
+//! This library works with ECMA-262 grammar definition, both for parsing and
+//! execution of static&dynamic semantics. So here is a reminder on grammar
+//! terminology used in the specification.
+//!
+//! Each grammar rule looks like `Production :: ProductionDefinition`. Each
+//! production has an algorithm for each static and dynamic semantics.
 
 mod _tokenizer;
 
