c2e2bb7eee1131f1c5978c49d8c218c357de0a1e
Oleg Iarygin <oleg@arhadthedev.net>
2024-06-04 21:12:41 +0400

    Fix clippy::needless-pass-by-value
    ```plain
    error: this argument is passed by value, but not consumed in the function body
      --> src/lib.rs:49:19
       |
    49 | fn process(token: Pair<lexical::Rule>) -> Token {
       |                   ^^^^^^^^^^^^^^^^^^^ help: consider taking a reference instead: `&Pair<lexical::Rule>`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#needless_pass_by_value
       = note: `-D clippy::needless-pass-by-value` implied by `-D warnings`
       = help: to override `-D warnings` add `#[allow(clippy::needless_pass_by_value)]`
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -40,15 +40,16 @@ pub fn get_next_token(input: &str) -> Result<(Token, &str), String> {
             let token = tokens.nth_back(0).unwrap(); 
             let token_size = token.as_str().len();
             let tail = &input[token_size..];
-            Ok((calculate(token), tail))
+            Ok((calculate(&token), tail))
         },
         Err(error) => Err(error.to_string())
     }
 }
 
-fn calculate(token: Pair<lexical::Rule>) -> Token {
+fn calculate(token: &Pair<lexical::Rule>) -> Token {
+    let value = token.as_str();
     match token.as_rule() {
-        lexical::Rule::DecimalDigit => Token::NumericLiteral(token.as_str().parse::<f64>().unwrap()),
+        lexical::Rule::DecimalDigit => Token::NumericLiteral(value.parse::<f64>().unwrap()),
     }
 }
 
